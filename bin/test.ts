8d8aef92c48000e2fe403607433eca9a181d1735
Julien <julien@ripouteau.com>
2023-07-03 00:28:53 +0200

    refactor: update to latest japa

--- bin/test.ts
+++ bin/test.ts
@@ -1,40 +1,12 @@
 import { assert } from '@japa/assert'
-import { specReporter } from '@japa/spec-reporter'
 import { fileSystem } from '@japa/file-system'
-import { runFailedTests } from '@japa/run-failed-tests'
-import { processCliArgs, configure, run } from '@japa/runner'
-import { pathToFileURL } from 'node:url'
+import { processCLIArgs, configure, run } from '@japa/runner'
 import { BASE_URL } from '../test_helpers/index.js'
 
-/*
-|--------------------------------------------------------------------------
-| Configure tests
-|--------------------------------------------------------------------------
-|
-| The configure method accepts the configuration to configure the Japa
-| tests runner.
-|
-| The first method call "processCliArgs" process the command line arguments
-| and turns them into a config object. Using this method is not mandatory.
-|
-| Please consult japa.dev/runner-config for the config docs.
-*/
+processCLIArgs(process.argv.slice(2))
 configure({
-  ...processCliArgs(process.argv.slice(2)),
-  ...{
-    files: ['tests/**/*.spec.ts'],
-    plugins: [assert(), runFailedTests(), fileSystem({ basePath: BASE_URL })],
-    reporters: [specReporter()],
-    importer: (filePath: string) => import(pathToFileURL(filePath).href),
-  },
+  files: ['tests/**/*.spec.ts'],
+  plugins: [assert(), fileSystem({ basePath: BASE_URL })],
 })
 
-/*
-|--------------------------------------------------------------------------
-| Run tests
-|--------------------------------------------------------------------------
-|
-| The following "run" method is required to execute all the tests.
-|
-*/
 run()
